3621a5a5d8cb1224013f764bbb0d78af5570fd51
ali-charara <ali.charar@hotmail.fr>
2023-05-05 16:44:53 +0200

    feat: A2C for discrete action space

---
+++ agents/base_agent/models.py
@@ -0,0 +1,41 @@
+import torch
+from torch import nn
+
+
+class ActorNetwork(nn.Module):
+    """
+    A network for actor
+    """
+
+    def __init__(self, state_dim, hidden_size, output_size, output_act):
+        super(ActorNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
+        # activation function for the output
+        self.output_act = output_act
+
+    def __call__(self, state):
+        out = nn.functional.tanh(self.fc1(state))
+        out = nn.functional.tanh(self.fc2(out))
+        out = self.output_act(self.fc3(out))
+        return out
+
+
+class CriticNetwork(nn.Module):
+    """
+    A network for critic
+    """
+
+    def __init__(self, state_dim, action_dim, hidden_size, output_size=1):
+        super(CriticNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_size)
+        self.fc2 = nn.Linear(hidden_size + action_dim, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
+
+    def __call__(self, state, action):
+        out = nn.functional.tanh(self.fc1(state))
+        out = torch.cat([out, action], 1)
+        out = nn.functional.tanh(self.fc2(out))
+        out = self.fc3(out)
+        return out
