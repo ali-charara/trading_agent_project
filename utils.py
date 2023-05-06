dfb9a5863934abd4b3924696748f7f985745103e
ali-charara <ali.charar@hotmail.fr>
2023-05-07 00:56:33 +0200

    feat: min variance portfolio & experiments notebook

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 import yfinance as yf
+from scipy import optimize
 
 
 def load_stocks(
@@ -18,7 +20,7 @@ def load_stocks(
 
 def number_to_base(n, b, l):
     if n == 0:
-        return [0]
+        return [0] * l
 
     digits = []
     while n:
@@ -29,3 +31,29 @@ def number_to_base(n, b, l):
         digits.append(0)
 
     return digits[::-1]
+
+
+def default_constraint(w, linear_constraint, bounds):
+    n = w.shape[0]
+    if linear_constraint is None:
+        linear_constraint = optimize.LinearConstraint([[1] * n], [1], [1])
+    if bounds is None:
+        bounds = optimize.Bounds([0] * n, [1] * n)
+
+    return linear_constraint, bounds
+
+
+def compute_mv_weights(
+    w: np.ndarray, cov_matrix: np.ndarray, linear_constraint=None, bounds=None
+) -> np.ndarray:
+    linear_constraint, bounds = default_constraint(w, linear_constraint, bounds)
+
+    def cost(w):
+        return w.T.dot(cov_matrix).dot(w)
+
+    res = optimize.minimize(
+        fun=cost, x0=w, constraints=linear_constraint, bounds=bounds
+    )
+    w = res.x
+
+    return w
